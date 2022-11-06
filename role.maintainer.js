a6d31eae862fae670b21153ec934908f0dc8706d
V13Axel <alex@stechstudio.com>
2022-11-06 01:00:25 -0400

    Updates, working maintainer

--- role.maintainer.js
+++ role.maintainer.js
@@ -18,6 +18,8 @@ var roleMaintainer = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
+        creep.say(creep.memory.action);
+
         switch(creep.memory.action) {
             case 'cleanup':
                 this.cleanup(creep);
@@ -58,12 +60,25 @@ var roleMaintainer = {
         if(!creep.memory.source) {
             let sources = creep.room.find(FIND_SOURCES);
 
-            creep.memory.source = sources[Math.floor(Math.random()*sources.length)];
+            creep.memory.source = sources[Math.floor(Math.random()*sources.length)].id;
+
+            console.log(creep.memory.source);
         }
 
 
-        if(creep.harvest(creep.memory.source) == ERR_NOT_IN_RANGE) {
-            creep.moveTo(creep.memory.source, {visualizePathStyle: {stroke: '#ffaa00'}});
+        let target = Game.getObjectById(creep.memory.source);
+        let attempt = creep.harvest(target); 
+        switch (attempt) {
+            case ERR_NOT_IN_RANGE:
+                creep.moveTo(target, {visualizePathStyle: {stroke: '#ffaa00'}});
+                break;
+            case ERR_INVALID_TARGET:
+                console.log("Invalid target", attempt, JSON.stringify(target));
+                creep.memory.source = null;
+                break;
+            default:
+                console.log(attempt);
+                
         }
     },
 
@@ -74,36 +89,17 @@ var roleMaintainer = {
             }
         });
         creep.moveTo(spawns[0]);
-    },
-
-    fix: function(creep) {
-        let attempt = creep.repair(creep.memory.fixing);
-
-        switch (attempt) {
-            case ERR_NOT_IN_RANGE:
-                creep.moveTo(creep.memory.fixing);
-                break;
-            default:
-                console.log(creep.name + "Repair attempt returned code: " + attempt);
-                break;
-        }
-
-        let targets = creep.room.find(FIND_STRUCTURES, { filter: creep.memory.fixing});
-        
-        if(targets.length != 1){
-            creep.memory.fixing = null;
-        }
 
         var needingRepair = creep.room.find(FIND_STRUCTURES, {
             filter: (structure) => {
                 return (structure.structureType == STRUCTURE_ROAD ||
                         structure.structureType == STRUCTURE_WALL ||
                         structure.structureType == STRUCTURE_RAMPART) && 
-                        (structure.hits < structure.hitsMax * 0.7 && 
-                        structure.hitsMax - structure.hits < creep.store.getUsedCapacity()
-                        );
+                        (structure.hits < structure.hitsMax * 0.95);
             }
         });
+
+        console.log(needingRepair.length);
 
         if(needingRepair.length < 1) {
             creep.memory.action = 'harvest';
@@ -111,7 +107,31 @@ var roleMaintainer = {
         }
 
         creep.memory.action = 'fixing';
-        creep.memory.fixing = needingRepair[0];
+        console.log(needingRepair[0])
+        creep.memory.fixing = needingRepair[0].id;
+    },
+
+    fix: function(creep) {
+        let targetStructure = Game.getObjectById(creep.memory.fixing);
+        let attempt = creep.repair(targetStructure);
+
+        switch (attempt) {
+            case ERR_INVALID_TARGET:
+                creep.memory.action = 'waiting';
+                break;
+            case ERR_NOT_IN_RANGE:
+                creep.moveTo(targetStructure);
+                break;
+            case ERR_NOT_ENOUGH_RESOURCES:
+                creep.memory.action = 'harvest';
+                break;
+            case OK:
+                creep.memory.fixing = null;
+                break;
+            default:
+                console.log(creep.name + "Repair attempt returned code: " + attempt);
+                break;
+        }
 
         return;
     }
