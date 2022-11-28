3976418a143e2acd58f6272a569d9646e8ffe638
V13Axel <alex@stechstudio.com>
2022-11-28 08:51:28 -0500

    Fix accidental queue flood

--- src/manager/task.rs
+++ src/manager/task.rs
@@ -1,6 +1,6 @@
 use std::{collections::HashMap, cmp::Ordering};
 
-use log::{debug};
+use log::{debug, info};
 use screeps::{Room, Creep, SharedCreepProperties, find, HasTypedId, HasId};
 
 use crate::{mem::{GameMemory, CreepMemory}, util::{self, screeps::Screeps}, minion::{MinionType, Minions}, task::{upgrade::Upgrade, harvest::Harvest, TaskProps, Action}};
@@ -70,9 +70,20 @@ impl TaskManager {
 
     fn _room_upgrade_tasks(room: &Room, game_memory: &mut GameMemory) {
         let upgrading_creeps = Screeps::get_screeps_doing(Upgrade::for_room(room), game_memory);
-        let needed_tasks = 5 - upgrading_creeps.len();
+        let total_tasks = 5 - upgrading_creeps.len();
 
         let room_tasks = game_memory.room_task_queues.entry(room.name().to_string()).or_default().entry(MinionType::Upgrader).or_default();
+        let needed_tasks = std::cmp::max(0, total_tasks - room_tasks.len());
+
+        if room_tasks.len() > 5 || needed_tasks > 5 {
+            info!("room_tasks length is {}, needed tasks is {}. One of those means we should clear the queue.", room_tasks.len(), needed_tasks);
+            // We messed up
+            room_tasks.clear();
+            return;
+        }
+
+        info!("Upgrade needs {} tasks: (5 - {}) - {}", needed_tasks, upgrading_creeps.len(), room_tasks.len());
+
         for _ in 1..needed_tasks {
             room_tasks.push(
                 Upgrade::for_room(room)
@@ -84,6 +95,8 @@ impl TaskManager {
         let spawn = &room.find(find::MY_SPAWNS)[0];
 
         let mut sources = room.find(find::SOURCES);
+        let mut max_possible_sources = 0;
+
 
         sources.sort_by(|a, b| {
             if spawn.pos().get_range_to(a) > spawn.pos().get_range_to(b) {
@@ -96,14 +109,30 @@ impl TaskManager {
         // Sources to harvest
         for source in sources.iter() {
             let spaces_available = util::position::PositionCalculator::spaces_around(&room, source.pos());
-            let harvesting_creeps = Screeps::get_screeps_doing(Action::Harvest(source.id()), game_memory);
-            let needed_tasks = spaces_available - harvesting_creeps.len();
+            max_possible_sources += spaces_available;
 
-            let room_harvester_tasks = game_memory.room_task_queues.entry(room.name().to_string()).or_default().entry(MinionType::Harvester)
+            let working_creeps = Screeps::get_screeps_doing(Action::Harvest(source.id()), game_memory);
+            let room_tasks = game_memory
+                .room_task_queues
+                .entry(room.name().to_string())
+                .or_default()
+                .entry(MinionType::Harvester)
                 .or_default();
+            let total_tasks = spaces_available - working_creeps.len();
+
+            let needed_tasks = std::cmp::max(0, total_tasks - room_tasks.len());
+
+
+            info!("Harvest needs {} tasks: ({} - {}) - {}", needed_tasks, spaces_available, working_creeps.len(), room_tasks.len());
 
             for _ in 1..needed_tasks {
-                room_harvester_tasks.push(Action::Harvest(source.id()));
+                room_tasks.push(Action::Harvest(source.id()));
+            }
+            if room_tasks.len() > max_possible_sources {
+                info!("room_tasks length is {}, max_possible is {}. Clearing the harvester queue.", room_tasks.len(), max_possible_sources);
+                // We messed up
+                room_tasks.clear();
+                return;
             }
         }
     }
